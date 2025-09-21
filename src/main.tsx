2d6d8290edb3026eae746081d21e7394ac610d4e
Antonio Bernardino da Silva <contato@antoniobsilva.com.br>
2025-09-21 02:07:52 -0300

    fix: version vercel analystics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 
